4d059d0c160a274a77adb930d5ddc9a7996571f8
Arjun Ajith <tparjunajith@gmail.com>
2018-05-04 09:53:40 +0530

    corrected string reverse function

--- internal/helpers/string_helper.go
+++ internal/helpers/string_helper.go
@@ -1,8 +1,42 @@
 package helpers
 
-func Reverse(str string) string {
-	if str != "" {
-		return Reverse(str[1:]) + str[:1]
+import "unicode"
+
+func Reverse(text string) string {
+	textRunes := []rune(text)
+	textRunesLength := len(textRunes)
+	if textRunesLength <= 1 {
+		return text
 	}
-	return ""
+
+	i, j := 0, 0
+	for i < textRunesLength && j < textRunesLength {
+		j = i + 1
+		for j < textRunesLength && isMark(textRunes[j]) {
+			j++
+		}
+
+		if isMark(textRunes[j-1]) {
+			// Reverses Combined Characters
+			reverse(textRunes[i:j], j-i)
+		}
+
+		i = j
+	}
+
+	// Reverses the entire array
+	reverse(textRunes, textRunesLength)
+
+	return string(textRunes)
+}
+
+func reverse(runes []rune, length int) {
+	for i, j := 0, length-1; i < length/2; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+}
+
+// isMark determines whether the rune is a marker
+func isMark(r rune) bool {
+	return unicode.Is(unicode.Mn, r) || unicode.Is(unicode.Me, r) || unicode.Is(unicode.Mc, r)
 }
